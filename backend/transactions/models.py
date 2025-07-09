45d2bc69f9a039268604cbf008e3c91ae869ae50
Saswat <ss.saswatsahoo@gmail.com>
2025-07-09 17:18:51 +0530

    Added ORM data model

--- backend/transactions/models.py
+++ backend/transactions/models.py
@@ -1,5 +1,8 @@
 from pydantic import BaseModel
-from sqlalchemy import Column, String, Float
+import sqlalchemy
+from sqlalchemy import Column, String, Float, DateTime
+from backend.utils.connectors import DB_BASE
+
 class Transaction(BaseModel):
     thread_id: str
     amount: float
@@ -9,3 +12,18 @@ class Transaction(BaseModel):
     destination: str
     mode: str
     reference_number: str = None
+
+class TransactionORM(DB_BASE):
+    __tablename__ = 'transactions'
+    __table_args__ = {'extend_existing': True}  
+    id = sqlalchemy.Column(String, primary_key=True)
+    amount = Column(Float)
+    transaction_type = Column(String(16))
+    source_identifier = Column(String(64))
+    source_type = Column(String(32))
+    destination = Column(String(128))
+    date_time = Column(DateTime)
+    mode = Column(String(32))
+    emailSender = Column(String(128))
+    emailId = Column(String(128))
+    reference_number = Column(String(64))
