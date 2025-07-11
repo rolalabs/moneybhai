bb906c55fc2635653af732610d790e4eeb4e580d
Saswat <ss.saswatsahoo@gmail.com>
2025-07-11 18:18:29 +0530

    Updated transaction data model

--- backend/transactions/models.py
+++ backend/transactions/models.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 import sqlalchemy
 from sqlalchemy import Column, String, Float, DateTime
-from backend.utils.connectors import DB_BASE
+from backend.utils.connectors import DB_BASE, DB_SESSION
 
 class Transaction(BaseModel):
     thread_id: str
@@ -18,12 +18,14 @@ class TransactionORM(DB_BASE):
     __table_args__ = {'extend_existing': True}  
     id = sqlalchemy.Column(String, primary_key=True)
     amount = Column(Float)
-    transaction_type = Column(String(16))
-    source_identifier = Column(String(64))
-    source_type = Column(String(32))
+    transaction_type = Column(String(128))
+    source_identifier = Column(String(128))
+    source_type = Column(String(128))
     destination = Column(String(128))
-    mode = Column(String(32))
+    mode = Column(String(128))
     date_time = Column(DateTime)
     emailSender = Column(String(128))
     emailId = Column(String(128))
-    reference_number = Column(String(64))
+    reference_number = Column(String(128))
+
+DB_BASE.metadata.create_all(DB_SESSION.bind)  # Create tables if they don't exist
