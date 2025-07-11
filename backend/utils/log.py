aa2178b737071a110db73dd9332afda33e3ceb61
Saswat <ss.saswatsahoo@gmail.com>
2025-07-11 18:19:14 +0530

    udpatedlog to print in the file for persistence

--- backend/utils/log.py
+++ backend/utils/log.py
@@ -2,10 +2,14 @@
 import logging
 
 def setup_logger(name="app", level=logging.INFO):
+    '''
+    Maintain a log file and append all the logs to that file
+    '''
+
     logger = logging.getLogger(name)
     if not logger.handlers:
         formatter = logging.Formatter("[%(asctime)s] [%(levelname)s] %(message)s")
-        handler = logging.StreamHandler()
+        handler = logging.FileHandler("app.log")
         handler.setFormatter(formatter)
         logger.addHandler(handler)
         logger.setLevel(level)
