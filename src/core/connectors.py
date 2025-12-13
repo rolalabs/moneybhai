6280f8e40202ea7a145fb7c427d2e3b68acb1166
Saswat <ss.saswatsahoo@gmail.com>
2025-12-13 19:11:03 +0530

    Refactor connectors.py to use environment settings for database configuration and API client initialization

--- src/core/connectors.py
+++ src/core/connectors.py
@@ -1,28 +1,57 @@
-import outlines
 from google import genai
-import os
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import json
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+from typing import Generator
+from google import genai
+from google.genai.types import HttpOptions
+from google.oauth2 import service_account
 
-load_dotenv()
+class Settings(BaseSettings):
+    DEBUG: bool = False
+    CLOUD_SQL_USER: str
+    CLOUD_SQL_PASSWORD: str
+    CLOUD_SQL_HOST: str
+    CLOUD_SQL_DB: str
+    API_TOKEN_GITHUB: str = None
+    GOOGLE_APPLICATION_CREDENTIALS: str = None
 
-GEN_AI_CLIENT = genai.Client(api_key=os.getenv('GENAI_API_KEY'))
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+ENV_SETTINGS = Settings()
+
+
+GEN_AI_CLIENT = genai.Client(api_key=ENV_SETTINGS.GEMINI_API_KEY, http_options=HttpOptions(timeout=60))
 
 # Initialize the Gemini model
 # MODEL = outlines.from_gemini(GEN_AI_CLIENT, "gemini-2.5-flash")
 
+DB_BASE = declarative_base(metadata=MetaData(schema="moneybhai"))
+DATABASE_URL = f"postgresql+psycopg2://{ENV_SETTINGS.CLOUD_SQL_USER}:{ENV_SETTINGS.CLOUD_SQL_PASSWORD}@{ENV_SETTINGS.CLOUD_SQL_HOST}/{ENV_SETTINGS.CLOUD_SQL_DB}"
 
-def get_db():
-    CLOUD_SQL_USER = 'reverie'
-    CLOUD_SQL_PASSWORD = 'postgres'
-    CLOUD_SQL_HOST = 'localhost:5432'  
-    CLOUD_SQL_DB = 'postgres'
+DB_ENGINE = create_engine(
+    DATABASE_URL,
+    echo=False,  # optional, logs SQL queries
+    future=True
+)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=DB_ENGINE
+)
 
-    DATABASE_URL = f"postgresql+psycopg2://{CLOUD_SQL_USER}:{CLOUD_SQL_PASSWORD}@{CLOUD_SQL_HOST}/{CLOUD_SQL_DB}"
-    engine = create_engine(DATABASE_URL)
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    return SessionLocal()
 
-DB_SESSION = get_db()
-DB_BASE = declarative_base()
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db  # provides the session to route
+    finally:
+        db.close()  # ensures session is closed after request
+
+credentials = service_account.Credentials.from_service_account_info(
+    json.loads(ENV_SETTINGS.GOOGLE_APPLICATION_CREDENTIALS)
+)
+credentials = credentials.with_scopes(["https://www.googleapis.com/auth/cloud-platform"])
