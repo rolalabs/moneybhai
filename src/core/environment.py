24409e78b6cd876f3aa023c4042450ae6d7bf5d2
Saswat <ss.saswatsahoo@gmail.com>
2026-01-06 23:37:02 +0530

    Make optional environment variables for API keys and URLs in settings

--- src/core/environment.py
+++ src/core/environment.py
@@ -2,11 +2,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DEBUG: bool = False
-    GEMINI_API_KEY: str
-    WORKER_CLOUD_RUN_URL: str
-    DATABASE_URL: str
-    API_TOKEN_GITHUB: str = None
-    GCP_CREDENTIALS: str = None
+    GEMINI_API_KEY: str | None = None
+    WORKER_CLOUD_RUN_URL: str | None = None
+    API_TOKEN_GITHUB: str | None = None
+    GCP_CREDENTIALS: str | None = None
+    DATABASE_URL: str | None = None
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
