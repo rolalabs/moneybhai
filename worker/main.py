b05bd202040be903b10be5b3336b17c80562cd57
Saswat <ss.saswatsahoo@gmail.com>
2026-01-22 05:58:33 +0530

    Limit email fetch to a single message per batch in processTask function

--- worker/main.py
+++ worker/main.py
@@ -115,7 +115,7 @@ async def processTask(request: Request, db: Session = Depends(get_db)):
         latest_email_time = None
         while True:
             # Fetch emails in batches
-            messages, next_page_token = emailManager.fetch_emails_messages_list(query, next_page_token, max_results=10)
+            messages, next_page_token = emailManager.fetch_emails_messages_list(query, next_page_token, max_results=1)
             logger.info(f"Fetched {len(messages)} emails for accountId: {tasksPayload.accountId}")
             processed_messages: list[EmailMessage] = emailManager.fetch_messages_details_list(messages)
             logger.info(f"Processed {len(processed_messages)} emails for accountId: {tasksPayload.accountId}")
@@ -146,6 +146,7 @@ async def processTask(request: Request, db: Session = Depends(get_db)):
                         latest_email_time = max(msg.date_time, latest_email_time)
             if not next_page_token:
                 break
+            break
         # while loop ends
 
         if latest_email_time:
