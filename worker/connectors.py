8b7c91a0e39683379e23f2f81935adab98b6015e
Saswat <ss.saswatsahoo@gmail.com>
2026-01-26 19:24:00 +0530

    Remove DATABASE_URL from settings and clean up unused database code

--- worker/connectors.py
+++ worker/connectors.py
@@ -1,18 +1,12 @@
 import json
 from google import genai
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import sessionmaker, declarative_base, Session
-from typing import Generator
 from google.oauth2 import service_account
 from google.genai.types import HttpOptions
 
 class Settings(BaseSettings):
     DEBUG: bool = False
     WORKER_CLOUD_RUN_URL: str
-    DATABASE_URL: str
     MB_BACKEND_API_URL: str = "http://0.0.0.0:8080/api/"
     API_TOKEN_GITHUB: str = None
     GCP_CREDENTIALS: str = None
@@ -40,27 +34,6 @@ VERTEXT_CLIENT = genai.Client(
     location="asia-south1",
     credentials=credentials,
 )
-
-DB_BASE = declarative_base()
-
-DB_ENGINE = create_engine(
-    ENV_SETTINGS.DATABASE_URL,
-    echo=False,  # optional, logs SQL queries
-    future=True
-)
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=DB_ENGINE
-)
-
-
-def get_db() -> Generator[Session, None, None]:
-    db = SessionLocal()
-    try:
-        yield db  # provides the session to route
-    finally:
-        db.close()  # ensures session is closed after request
 
 # credentials = service_account.Credentials.from_service_account_info(
 #     json.loads(ENV_SETTINGS.GOOGLE_APPLICATION_CREDENTIALS)
