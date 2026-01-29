043e5b11279c22daeee8d80b106c6e05475f848f
Saswat <ss.saswatsahoo@gmail.com>
2026-01-29 21:31:54 +0530

    Add budgets router to API and create budgets module

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from src.api.v1 import emails, admin, users, transactions, analytics, accounts, orders, chotu
+from src.api.v1 import emails, admin, users, transactions, analytics, accounts, orders, chotu, budgets
 
 api_router = APIRouter()
 api_router.include_router(emails.router, prefix="/emails", tags=["emails"])
@@ -10,3 +10,4 @@ api_router.include_router(transactions.router)
 api_router.include_router(analytics.router)
 api_router.include_router(orders.router)
 api_router.include_router(chotu.router)
+api_router.include_router(budgets.router)
